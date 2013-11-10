c632eb21dbcf49e4e57d55636641d3bd0528be83
lynda wellhausen <lyndawellhausen@gmail.com>
2013-11-10 02:24:23 -0600

    start update collection

--- mongo.rb
+++ mongo.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'mongo'
 require 'uri'
 require 'json'
-include Mongo
+#require 'info'
+
+
 
 # db = mongo_client.db("app16243887")
 # db = MongoClient.new #("paulo.mongohq.com", 10029).db("app16243887")
@@ -16,7 +18,36 @@ def get_connection
   @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
   @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
   @db_connection
-  puts db_name
 end
 
-puts 'hello'
+
+def return_collection
+  trippr_array = []
+  db = get_connection
+  collections = db.collection_names()
+  last_collection = collections[-1]
+  coll = db.collection(last_collection)
+
+  # return each collection and add it to docs
+  docs = coll.find()  
+
+  # add each collection to the trippr_array
+  docs.each{ |doc| trippr_array << doc.to_json }
+
+  # once all collections are added return the trippr_array
+  return trippr_array
+
+  # trippr_array.each do |collection|
+  #   puts collection
+  # end
+end
+
+def add_to_collection(new_record = {})
+  #new_record.each |key, value|
+
+  id = @daytrippr.insert('#{new_record}')
+
+  #{"_id":
+  #{"$oid": "527f06d6584b2141453d21aa"},"Name of Town/Location":"New Orleans, LA","Coordinates":"29.951365,-90.076332"}
+end
+
